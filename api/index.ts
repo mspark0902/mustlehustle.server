b0948a6608e4cc08b4d8b1902ef86ec6f3f31873
mspark-admin <santhoshm@msspark.in>
2024-06-22 16:14:06 +0530

    ds

--- api/index.ts
+++ api/index.ts
@@ -32,7 +32,7 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
 
 app.get('/users', async (req: Request, res: Response) => {
